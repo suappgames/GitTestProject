d08919c691bace35e98e9ce32f5c066a53dc7ca7
The New <thenewarchitech@gmail.com>
2020-04-26 12:33:36 +0300

    Initial Commit

---
+++ GitTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by SEDAT on 4/26/20.
+//  Copyright © 2020 thenewarchitech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
